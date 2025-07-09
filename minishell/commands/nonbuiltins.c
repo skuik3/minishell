3dc544a50cbefaf5f928a037257cb19cc04766bb
Anežka Havránková <anezkahavrankova@Anezka-MacBook-Air.local>
2025-07-09 10:02:38 +0200

    executing command ok

--- minishell/commands/nonbuiltins.c
+++ minishell/commands/nonbuiltins.c
@@ -6,7 +6,7 @@
 /*   By: anezkahavrankova <anezkahavrankova@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 15:27:14 by anezkahavra       #+#    #+#             */
-/*   Updated: 2025/07/08 17:03:03 by anezkahavra      ###   ########.fr       */
+/*   Updated: 2025/07/09 10:02:15 by anezkahavra      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 
 // needed commands recognition
 // very very basic functionality
+
 // maybe check with acces if file exist >> possible error
+// maybe check status if child process works correctly
 int executing(char *str, char *evnp[])
 {       
      //now defined, later when tokenization works, should be generally usable
     char *const argv[] = {"ls", "-l", NULL}; //depends on tokenization from readline, do later
-    
-    execve("/bin/ls", argv, evnp); 
+    int pid;
+    int status;
+
+    pid = fork();
+    if (pid < -1)
+    {
+        write(STDERR_FILENO, "Error forking\n", 15);
+        return (1);
+    }
+    else if (pid == 0)
+        execve("/bin/ls", argv, evnp); 
+    else if (pid > 0)
+        waitpid(pid, &status, 0);
     return (0);
 }
-//finishes process
-//probably neeed to use fork()
+
