174945d361be0754951bd7ca6a75e89451e84678
Anežka Havránková <anezkahavrankova@Anezka-MacBook-Air.local>
2025-07-16 23:10:41 +0200

    added other files

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -6,7 +6,7 @@
 /*   By: anezkahavrankova <anezkahavrankova@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 12:38:30 by anezkahavra       #+#    #+#             */
-/*   Updated: 2025/07/08 11:54:48 by anezkahavra      ###   ########.fr       */
+/*   Updated: 2025/07/16 23:05:35 by anezkahavra      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,40 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+typedef struct environment_variables
+{
+    char **start;
+    char **mod;
+} env_t;
+
+
 //builtin fce
-int what_builtin(char *argv, char *envp[]);
+int what_builtin(char *argv, env_t *env);
 int run_pwd(void);
 int run_echo(char **string);
-int run_cd(char *path);
-int run_env(char *envp[]);
-int run_exit(void);
-int run_export(char *envp[], char *arguments);
+int run_cd(char *path); //todo home
+int run_env(char **envp);
+int run_exit(void); //todo s ciselkami
+int run_export(env_t *env, char *arguments);
+int run_unset(env_t *env, char *arguments); 
+
+//utils
+int saving_env(char ***env, char *envp[]);
+int copy_string(char **env, char *orig_env);
+int find_start(char *envp[], char *arguments);
+int counting_envlen(char **envp);
+char  **put_envp(char **old_envp, char *new_arg);
+int get_order(char **envp);
+char *find_variable(char *arguments);
+char **put_unset(char **old_env, int unset);
+int unset_variable(char *envp, char *variable, int i);
+void	ft_putstr_fd(char *s, int fd);
 
 int redirecting_in(char *str);
 int redirecting_out(char *str);
 int appending(char *str);
+
+int executing(char *str, char *evnp[]);
 
 //libft_later
 int	ft_strcmp(const char *s1, const char *s2);
@@ -44,5 +66,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 //for readability
 #define SHELL_DEFAULT 00644
+
+#define ERR_MALLOC "Malloc failure\n"
+#define ERR_ARG "Not enough arguments\n"
 
 #endif
