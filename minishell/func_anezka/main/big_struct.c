7b391c2886a7b7786d02820ec24be63d1c6aee51
Anežka Havránková <anezkahavrankova@Anezka-MacBook-Air.local>
2025-09-05 12:19:43 +0200

    fixed small issue with eof

---
+++ minishell/func_anezka/main/big_struct.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   big_struct.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anezkahavrankova <anezkahavrankova@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/05 11:44:46 by anezkahavra       #+#    #+#             */
+/*   Updated: 2025/09/05 12:10:01 by anezkahavra      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+// t_bigie *setting_big(void)
+// {
+//     t_bigie *new;
+
+//     new = malloc(sizeof(t_bigie));
+//     if (new == NULL)
+//     {
+//         perror("");
+//         return (NULL);
+//     }
+//     new->cmd = NULL;
+//     new->env = NULL;
+//     new->exit_status = NULL;
+//     new->pipes = NULL;
+//     // new->type = NULL;
+//     return(new);
+// }
